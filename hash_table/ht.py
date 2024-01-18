3c52f673c0193fd9645f6ab58dadf52dfbe2c95e
Munagala Sai Raghava <88725496+sairaghavamunagala@users.noreply.github.com>
2024-01-18 16:59:13 +0000

    Feat: Add hash set
    This commit adds set_item method
    to hash table.

--- hash_table/ht.py
+++ hash_table/ht.py
@@ -6,12 +6,22 @@ class HashTable:
     def __hash(self,key):
         my_hash=0
         for letter in key:
-            my_hash=(my_hash+ord(letter)*23)%len(data_map) # number can be any prime number
+            my_hash=(my_hash+ord(letter)*23)%len(self.data_map) # number can be any prime number
         return my_hash
+
+    def set_item(self,key,value):
+        index=self.__hash(key)
+        if self.data_map[index]==None:
+            self.data_map[index]=list()
+        self.data_map[index].append([key,value])
 
     def print_table(self):
         for i,val in enumerate(self.data_map):
             print(i,":",val)
 
 my_hash_table=HashTable()
+
+my_hash_table.set_item("bolts",400)
+my_hash_table.set_item("washers",50)
+my_hash_table.set_item("lumber",70)
 my_hash_table.print_table()
